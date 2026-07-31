0372efb738cc3abe657a750121bfd71436ecef41
itsemanuela <emanuela_carrubba_@hotmail.com>
2026-07-31 17:05:29 +0200

    inizializzazione progetto

---
+++ src/main/java/emanuela/carrubba/matillo_bakery/MatilloBakeryApplication.java
@@ -0,0 +1,13 @@
+package emanuela.carrubba.matillo_bakery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatilloBakeryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatilloBakeryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/emanuela/carrubba/matillo_bakery/MatilloBakeryApplicationTests.java
@@ -0,0 +1,13 @@
+package emanuela.carrubba.matillo_bakery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatilloBakeryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
