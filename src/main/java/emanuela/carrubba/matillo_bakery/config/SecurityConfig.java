d062965a09959c3bc657fcf848e3279f85b1ee9e
itsemanuela <emanuela_carrubba_@hotmail.com>
2026-09-02 18:53:51 +0200

    modifiche sec

--- src/main/java/emanuela/carrubba/matillo_bakery/config/SecurityConfig.java
+++ src/main/java/emanuela/carrubba/matillo_bakery/config/SecurityConfig.java
@@ -55,6 +55,7 @@ public class SecurityConfig {
 
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/prodotti", "/api/prodotti/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/auth/richiedi-reset").permitAll()
